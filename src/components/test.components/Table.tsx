6f7ec37f173f57eff4c6258f52c401f33276a902
victortarroni <victortarroni@gmail.com>
2024-07-25 21:24:37 +0100

    first edition on the time-table

--- src/components/test.components/Table.tsx
+++ src/components/test.components/Table.tsx
@@ -1,33 +1,41 @@
 import Table from 'react-bootstrap/Table';
+import './Table_style.css'
 
 function WeatherTable() {
   return (
     <Table striped bordered hover size="sm">
       <thead>
         <tr>
-          <th>#</th>
-          <th>First Name</th>
-          <th>Last Name</th>
-          <th>Username</th>
+          <th className='head-timer'>06h</th>
+          <th className='head-timer'>12h</th>
+          <th className='head-timer'>15h</th>
+          <th className='head-timer'>18h</th>
         </tr>
       </thead>
       <tbody>
         <tr>
-          <td>1</td>
-          <td>Mark</td>
-          <td>Otto</td>
-          <td>@mdo</td>
+          <td className='six-wave'></td>
+          <td className='twelve-wave'></td>
+          <td className='three-wave'></td>
+          <td className='eighteen-wave'></td>
         </tr>
         <tr>
-          <td>2</td>
-          <td>Jacob</td>
-          <td>Thornton</td>
-          <td>@fat</td>
+          <td className='six-tide'></td>
+          <td className='twelve-tide'></td>
+          <td className='three-tide'></td>
+          <td className='eighteen-tide'></td>
         </tr>
         <tr>
-          <td>3</td>
-          <td colSpan={2}>Larry the Bird</td>
-          <td>@twitter</td>
+        <td className='six-wind'></td>
+          <td className='twelve-wind'></td>
+          <td className='three-wind'></td>
+          <td className='eighteen-wind'></td>
+        </tr>
+        <tr>
+        <td className='six-temp'></td>
+          <td className='twelve-temp'></td>
+          <td className='three-temp'></td>
+          <td className='eighteen-temp'></td>
         </tr>
       </tbody>
     </Table>
